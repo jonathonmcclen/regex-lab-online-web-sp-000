1fbb79445926467ce88990f7e9c824c0275f0a9c
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-02 23:09:38 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.match(/^[aeiouAEIOU]/) ? true : false
+  word.match(/^[aeiouAEIOU]{1}/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -12,6 +12,7 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   text.match(/^[A-Z]{1}[a-z\s]+[?!.]/) ?  true : false
+  puts text
 end
 
 def valid_phone_number?(phone)
